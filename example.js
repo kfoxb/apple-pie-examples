70cf6d0be1d5ca8816e9ebc1e6e4c6994533406d
Kyle Bradford <kyle@watchtower.ai>
2019-03-22 18:04:06 -0700

    WIP: add some more examples and comments

--- example.js
+++ example.js
@@ -12,7 +12,12 @@ const getValue = (value = ERR) => {
 }
 
 const main = async () => {
-  console.log('in main')
+  // initial idea of api
+  // pros
+  // provides a way to access error and value
+  // cons
+  // error code repeats, may not be very clear for side effects
+  console.log('main')
   const value = await p(getValue());
   if (e(value)) {
     console.error("Error getting value", e(value))
@@ -21,5 +26,49 @@ const main = async () => {
   console.log("got value", value)
 }
 
-
 main()
+
+const main1 = async () => {
+  console.log('main1')
+  // example of returning a value or null
+  // pros
+  // simple to understand, short code
+  // cons
+  // lose error value, not sure how it would work for side effects
+  const value = await getValue().catch(() => null);
+  if (!value) {
+    console.error("Error getting value", value)
+    return
+  }
+  console.log("got value", value)
+}
+
+main1()
+
+const main2 = async () => {
+  // example of declaring a handler function before trying async values
+  // pros
+  // possible to define an error handler for multiple awaits, more dry
+  // access to error value
+  // should work with side effects
+  // cons
+  // not extremely clear that getting value2 will use the first error handler
+  // if p.e is made to return from main2, babel will have to be used and won't be as clear
+  console.log('main2')
+  p.e = (err) => {
+    console.error("Error getting value", value)
+    return
+  }
+  const value = await p(getValue());
+  const value2 = await p(getValue("value"));
+
+  p.e = () => {
+    console.error("Error getting third value", value)
+    return
+  }
+  const value3 = await p(getValue("value3"));
+
+  console.log("got values", value, value2, value3)
+}
+
+main2()
